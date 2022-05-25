3972003d2b10fce7bb2eddef8110a6ead66fcca7
pacanada <pereirapcanada@gmail.com>
2022-05-25 22:59:19 +0200

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # pytorch-m1-gpu
+## Getting started
+
+1. Install native python for m1 [miniconda3 MacOs m1](https://docs.conda.io/en/latest/miniconda.html)
+
+2. Run:
+
+```shell
+/Users/<username>/miniconda3/condabin/conda create -n torch-nightly python=3.8
+```
+```
+/Users/<username>/miniconda3/envs/torch-nightly/bin/pip3 install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
+```
+(to avoid configuring miniconda for native python as default)
+
+3. Run the script:
+
+```shell
+/Users/<username>/miniconda3/envs/torch-nightly/bin/python3 main.py
+```
+Alternatively you can activate the conda env with the cexisting conda instalation
+```shell
+conda activate /Users/pablocanadapereira/miniconda3/envs/torch-nightly
+```
+
+4. Check python version and if mps backend is supported
+```shell
+python -c 'import platform; print(platform.platform())'
+```
+```shell
+print(torch.backends.mps.is_available())
+```
+
+For now the performance is way worse..
+
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets
+from torch.utils.data import DataLoader
 
 class NN(nn.Module):
     def __init__(self):
@@ -14,6 +17,7 @@ class NN(nn.Module):
         x = self.fc2(x)
         return x
 
+
 m1_gpu = torch.device("mps")
 m1_cpu = torch.device("cpu")
 EPOCHS = 10
@@ -24,20 +28,33 @@ if GPU == True:
 else:
     device = m1_cpu
 
-X_all = torch.Tensor(np.random.randn(10, 20, 10)).to(device)
-y_all = torch.Tensor(np.random.randn(10, 20, 1)).to(device)
+print("torch", torch.__version__)
+print("device", device)
+print(torch.backends.mps.is_available())
+
+
+
+#X_all = torch.Tensor(np.random.randn(100, 200,10)).to(device)
+#y_all = torch.Tensor(np.random.randn(100, 200, 1)).to(device)
+X= torch.randn(10000, 200, 10, device=device)
+y = torch.randn(10000, 200, 1, device=device)
 
 
 # Train
+#model = NN_mps()
 model = NN().to(device)
 loss_f = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-6)
 model.train()
 
 for it in range(EPOCHS):
+
+
+
     #list_3dim = []
     for i in range(20):
-        X, y = X_all[:,i,:], y_all[:,i,:]
+        #X, y = X_all[:,i,:], y_all[:,i,:]
+        #X, y = X_all, y_all
         y_pred = model(X)
         #list_3dim.append(model.encoder(model.flatten(X)))
         
@@ -45,7 +62,8 @@ for it in range(EPOCHS):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        #time.sleep(1)
 
 
-print(loss.mean())
+#print(loss.mean())
 
